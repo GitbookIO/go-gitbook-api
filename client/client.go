7c430d18a643ec54d27b8cb15256f0283e356de4
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-08-22 12:53:56 -0700

    Make client package a better wrapping of napping

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"fmt"
 	"github.com/jmcvetta/napping"
 	"net/url"
 	"path"
@@ -29,9 +30,39 @@ func NewClient(opts ClientOptions) *Client {
 	}
 }
 
-// Wrap napping.Session.Send
-// So that we insert the client's host in the URL
-func (c *Client) Send(r *napping.Request) (*napping.Response, error) {
-	r.Url = path.Join(c.Host, r.Url)
-	return c.Session.Send(r)
+func (c *Client) Delete(url string, result, errMsg interface{}) (*napping.Response, error) {
+	return c.Session.Delete(c.Url(url), result, errMsg)
+}
+
+func (c *Client) Get(url string, p *napping.Params, result, errMsg interface{}) (*napping.Response, error) {
+	return c.Session.Get(c.Url(url), p, result, errMsg)
+}
+
+func (c *Client) Head(url string, result, errMsg interface{}) (*napping.Response, error) {
+	return c.Session.Head(c.Url(url), result, errMsg)
+}
+
+func (c *Client) Options(url string, result, errMsg interface{}) (*napping.Response, error) {
+	return c.Session.Options(c.Url(url), result, errMsg)
+}
+
+func (c *Client) Patch(url string, payload, result, errMsg interface{}) (*napping.Response, error) {
+	return c.Session.Patch(c.Url(url), payload, result, errMsg)
+}
+
+func (c *Client) Post(url string, payload, result, errMsg interface{}) (*napping.Response, error) {
+	return c.Session.Post(c.Url(url), payload, result, errMsg)
+}
+
+func (c *Client) Put(url string, payload, result, errMsg interface{}) (*napping.Response, error) {
+	return c.Session.Put(c.Url(url), payload, result, errMsg)
+}
+
+// Url returns the full http url including host
+func (c *Client) Url(urlpath string) string {
+	parsed, _ := url.Parse(c.Host)
+	parsed.Path = path.Join(parsed.Path, urlpath)
+	str := parsed.String()
+	fmt.Printf("Going to '%s'\n", str)
+	return str
 }
