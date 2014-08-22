e5153e8722960b292bcd264c563183e1da82e7df
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-08-22 12:29:40 -0700

    Add API Book manager

---
+++ api/book.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/GitbookIO/go-gitbook-api/client"
+	"github.com/GitbookIO/go-gitbook-api/models"
+)
+
+type Book struct {
+	Client *client.Client
+}
+
+// Get returns a books details for a given "bookId"
+// (for example "gitbookio/javascript")
+func (b *Book) Get(bookId string) (models.Book, error) {
+	book := models.Book{}
+
+	_, err := b.Client.Get(
+		fmt.Sprintf("/api/book/%s", bookId),
+		nil,
+		&book,
+		nil,
+	)
+
+	return book, err
+}
