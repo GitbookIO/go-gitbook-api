d968430fb895adb9ceaaef7a343c30a43f297f7b
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-08-22 18:12:31 -0700

    Add targz.go to utils

---
+++ utils/targz.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+	"os/exec"
+)
+
+var (
+	tarArgs = []string{"tar", "-cz", "-", "."}
+)
+
+// TarGz returns a stream of tar.gz data of the directory
+func TarGz(dir string) (io.ReadCloser, error) {
+	return tarCommand(tarArgs, dir)
+}
+
+// TarGzExclude returns a stream of tar.gz data of the directory
+// excluding the specified files
+func TarGzExclude(dir string, exclude ...string) (io.ReadCloser, error) {
+	return tarCommand(tarExcludeArgs(exclude...), dir)
+}
+
+// Run tar command for a folder given the provided args
+func tarCommand(args []string, dir string) (io.ReadCloser, error) {
+	cmd := exec.Command(args[0], args[1:]...)
+
+	// Set target directory
+	cmd.Dir = dir
+
+	// Get stream
+	return CmdStream(cmd, nil)
+}
+
+// Generate args for excluding files
+func tarExcludeArgs(files ...string) []string {
+	excluding := []string{}
+
+	for _, f := range files {
+		excluding = append("--exclude", f)
+	}
+
+	return append(
+		[]string{}, tarArgs[0],
+		append(excluding, tarArgs[1:]...)...,
+	)
+}
