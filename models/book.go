3cd784ab6c895cea9f63be92a84d2e3b6e148659
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-11-17 21:48:43 +0100

    Update book model with new urls structure

--- models/book.go
+++ models/book.go
@@ -2,11 +2,23 @@ package models
 
 // Books data structure as returned by the API
 type Book struct {
-	Id          string            `json:"id"`
-	Name        string            `json:"name"`
-	Title       string            `json:"title"`
-	Description string            `json:"description"`
-	Urls        map[string]string `json:"urls"`
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Urls        struct {
+		Access    string `json:"access"`
+		Homepage  string `json:"homepage"`
+		Read      string `json:"read"`
+		Reviews   string `json:"reviews"`
+		Subscribe string `json:"subscribe"`
+
+		Download struct {
+			Epub string `json:"epub"`
+			Mobi string `json:"mobi"`
+			Pdf  string `json:"pdf"`
+		} `json:"download"`
+	} `json:"urls"`
 
 	Author struct {
 		Name     string `json:"name"`
