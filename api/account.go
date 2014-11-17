d3a991d5f3e888d6a7cb7109f101257cec774370
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-11-17 21:49:06 +0100

    Implement API.Account.Get()

--- api/account.go
+++ api/account.go
@@ -2,8 +2,23 @@ package api
 
 import (
 	"github.com/GitbookIO/go-gitbook-api/client"
+	"github.com/GitbookIO/go-gitbook-api/models"
 )
 
 type Account struct {
 	Client *client.Client
+}
+
+// Get returns a books details for a given "bookId"
+// (for example "gitbookio/javascript")
+func (a *Account) Get() (models.Account, error) {
+	account := models.Account{}
+
+	_, err := a.Client.Get(
+		"/api/user/",
+		nil,
+		&account,
+	)
+
+	return account, err
 }
