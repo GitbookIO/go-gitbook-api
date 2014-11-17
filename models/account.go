ef7a90e79e15931c3d9db4e52627cf68cad2f55e
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-11-17 21:48:56 +0100

    Update Account model

--- models/account.go
+++ models/account.go
@@ -1,7 +1,9 @@
 package models
 
-// Books data structure as returned by the API
+// Account data structure as returned by the API
 type Account struct {
-	Username string `json:"username"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
 	Token    string `json:"token"`
+	Username string `json:"username"`
 }
