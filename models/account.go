c6128fd3576513eba2942a123d672531b08b85f2
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-12-03 11:25:45 +0100

    Extract GitHub info from GET:/api/user

--- models/account.go
+++ models/account.go
@@ -2,8 +2,14 @@ package models
 
 // Account data structure as returned by the API
 type Account struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Token    string `json:"token"`
+	Name     string             `json:"name"`
+	Email    string             `json:"email"`
+	Token    string             `json:"token"`
+	Username string             `json:"username"`
+	GitHub   *GitHubAccountInfo `json:"github"`
+}
+
+type GitHubAccountInfo struct {
 	Username string `json:"username"`
+	Token    string `json:"token"`
 }
