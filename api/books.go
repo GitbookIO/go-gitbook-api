140677bcc9d28f7c29c05ae5a78306f365442279
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-11-05 15:32:51 +0100

    Implement api.Books.List()
    Fixes #11

--- api/books.go
+++ api/books.go
@@ -2,8 +2,27 @@ package api
 
 import (
 	"github.com/GitbookIO/go-gitbook-api/client"
+	"github.com/GitbookIO/go-gitbook-api/models"
 )
 
 type Books struct {
 	Client *client.Client
+}
+
+type booksListResponse struct {
+	List []models.Book `json:"list"`
+}
+
+func (b *Books) List() ([]models.Book, error) {
+	resp := booksListResponse{}
+
+	if _, err := b.Client.Get(
+		"/books",
+		nil,
+		&resp,
+	); err != nil {
+		return nil, err
+	}
+
+	return resp.List, nil
 }
